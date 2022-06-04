53cd260c764cc17d5a56598f89ce85c999f20e08
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-06-04 13:28:16 +0300

    exe

--- Exe-DOM-Introduction/05. Table - Search Engine/solution.js
+++ Exe-DOM-Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,15 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
    function onClick() {
-      //   TODO:
-
+      let searchedField = document.getElementById('searchField');
+      let searchedFieldValue = searchedField.value;
+      searchedField.value = '';
+      let tableRows = document.querySelectorAll('table tbody tr');
+      for (const row of tableRows) {
+         row.classList.remove('select');
+         if(row.textContent.includes(searchedFieldValue) && searchedFieldValue != ''){
+            row.className = 'select';
+         }
+      }
    }
 }
