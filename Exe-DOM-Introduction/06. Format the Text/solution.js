592c740500c2b56585fa0ca5fe515361634381d5
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-06-05 13:18:15 +0300

    exercise

--- Exe-DOM-Introduction/06. Format the Text/solution.js
+++ Exe-DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+  let outputField = document.getElementById('output');
+  let inputField = document.getElementById('input').value;
+  let arrOfElements = inputField.split(".");
+  arrOfElements = arrOfElements.filter((x) => x.length > 0);
+  let currentP = '';
+  let counter = 0;
+  for (let i = 0; i < arrOfElements.length; i++) {
+    currentP += arrOfElements[i] + '.';
+    counter++;
+    if (counter == 3 || i == arrOfElements.length - 1) {
+      let para = document.createElement("p");
+      para.textContent = currentP;
+      currentP = '';
+      counter = 0;
+      outputField.appendChild(para);
+    }
+  }
 }
