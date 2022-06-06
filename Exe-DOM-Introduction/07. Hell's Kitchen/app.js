bca3db635b7720fc966d9dd40a9d028c21fde0fd
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-06-06 13:41:33 +0300

    exercise

--- Exe-DOM-Introduction/07. Hell's Kitchen/app.js
+++ Exe-DOM-Introduction/07. Hell's Kitchen/app.js
@@ -2,7 +2,44 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
-      
+      let objectsRestaurants = [];
+      let inputField = document.querySelector('#inputs textarea');
+      let outputParaRestaurants = document.querySelector('#bestRestaurant p');
+      let outputParaWorkers = document.querySelector('#workers p');
+      let inputArr = JSON.parse(inputField.value);
+      for (const currentRestaurant of inputArr) {
+         let currentObj = {};
+         let [restaurantName, ...employeesData] = currentRestaurant.split(' - '); 
+         for (const restaurant of objectsRestaurants) {
+            let currentRestaurantName = (Object.keys(restaurant))[0];
+            if(currentRestaurantName == restaurantName){
+               currentObj = restaurant;
+               break;
+            }
+         }
+         currentObj[restaurantName] = restaurantName;
+         employeesData = employeesData[0].split(/,* +/);
+         while (employeesData.length > 0) {
+            let [name, salary] = employeesData;
+            employeesData.shift();
+            employeesData.shift();
+            salary = Number(salary);
+            currentObj[name] = salary;
+         }
+            if(!objectsRestaurants.includes(currentObj)){
+               objectsRestaurants.push(currentObj);
+            }
+      }
+         for (const restaurant of objectsRestaurants) {
+            
+            let salaryArr = (Object.values(restaurant)).slice(1);
+            console.log(salaryArr);
+            let salarySum = salaryArr.reduce(function(a, b){
+               return a + b;
+           }, 0);
+            let avgSalary = (salarySum / salaryArr.length)
+            console.log(avgSalary);
+
+         }
    }
 }
