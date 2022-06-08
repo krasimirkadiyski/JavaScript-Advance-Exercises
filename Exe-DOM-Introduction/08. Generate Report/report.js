e0fc0d011f378366ea2902d7b55e5046419ae923
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-06-08 23:32:08 +0300

    exercises

--- Exe-DOM-Introduction/08. Generate Report/report.js
+++ Exe-DOM-Introduction/08. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+    
+    let arrCheckBoxes= Array.from(document.querySelectorAll('table thead tr th input[type=checkbox]'));
+    arrCheckBoxesNames = arrCheckBoxes.map((e) => e.name);
+    let allRows = Array.from(document.querySelectorAll(`table tbody tr`));
+    let resultArrReport = [];
+    for(let row = 0; row < allRows.length; row++){
+        let cols = allRows[row].children;
+        let obj = {};
+        for(let col = 0; col < cols.length; col++){
+            if(arrCheckBoxes[col].checked){
+                obj[arrCheckBoxesNames[col]] = cols[col].textContent;
+            }
+        }
+        resultArrReport.push(obj);
+    }
+
+
+    let textReport = JSON.stringify(resultArrReport);
+    let outputTextArea = document.getElementById('output');
+    outputTextArea.value = textReport;
 }
