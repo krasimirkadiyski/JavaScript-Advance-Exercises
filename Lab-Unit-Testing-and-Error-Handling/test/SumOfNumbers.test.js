d994dad3dcdb9c5f9d7f0099186824a4f1380f74
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-06-17 13:16:14 +0300

    exercises

--- Lab-Unit-Testing-and-Error-Handling/test/SumOfNumbers.test.js
+++ Lab-Unit-Testing-and-Error-Handling/test/SumOfNumbers.test.js
@@ -11,14 +11,3 @@ it('work with one element', () => {
     expect(sum([0])).to.equal(0);
 });
 });
-// describe('sum checker', () => {
-//     it ('works with array of numbers', () => {
-//         expect(sum([2, 2, 1])).to.equal(5);
-//     });
-//     it ('works with negative numbers', () => {
-//         expect(sum([-1, -3, 5])).to.equal(1);
-//     });
-//     it ('works with one number', () => {
-//         expect(sum([0])).to.equal(0);
-//     });
-// });
